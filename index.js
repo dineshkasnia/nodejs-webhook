440a249d682dfb942558d976f8f568a9090eb5eb
Dinesh Kasnia <dineshx33@gmail.com>
2023-05-20 14:20:22 +0530

    update eight

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ app.post("/webhook", (req, res) => {
 
     if(body_param.object){
         if(body_param.entry && 
-            body_param.entry[0].changes &&
-            body_param.entry[0].changes.value.hasOwnProperty(messages) &&
-            body_param.entry[0].changes.value.messages[0]
+            body_param.entry[0].changes
             ){
                 let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
                 let from = body_param.entry[0].changes[0].value.metadata.display_phone_number;
