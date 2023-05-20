feea4c1af96aeff3b5652d5acfbe5267f9e698c4
Dinesh Kasnia <dineshx33@gmail.com>
2023-05-20 11:57:17 +0530

    reinitialized files

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const body_parser = require('body-parser');
+const axios = require("axios");
+
+const app = express().use(body_parser.json());
+
+const token="no token here";
+const mytoken ="dinesh";
+
+app.listen(8000, () =>{
+    console.log('webhook is listening.');
+});
+
+app.get("/webhook", (req, res) => {
+    let mode = req.query["hub.mode"];
+    let challenge = req.query["hub.challenge"];
+    let token = req.query["hub.verify_token"];
+
+    
+
+    if(mode && token){
+        if(mode==='subscribe' && token===mytoken){
+            res.status(200).send(challenge);
+        }
+        else{
+            res.status(403)
+        }
+    }
+});
+
+
+app.post("/webhook", (req, res) => {
+    let body_param = req.body;
+
+    console.log(JSON.stringify(body_param, null, 2));
+
+    if(body_param.object){
+        if(body_param.entry && 
+            body_param.entry[0].changes &&
+            body_param.entry[0].changes.value.message &&
+            body_param.entry[0].changes.value.message[0]
+            ){
+                let phone_number_id = body_param.entry[0].changesgit[0].value.metadata.phone_number_id;
+                let from = body_param.entry[0].changes[0].value.messages[0].from;
+                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+
+                axios({
+                    method:'POST',
+                    url:"https://graph.facebook.com/v16.0/"+phone_number_id+"/messages?access_token="+token,
+                    data:{
+                        messaging_product:"whatsapp",
+                        to:from,
+                        text:{
+                            body:"Hi.. I am dinesh kasnia"
+                        }
+                    },
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                });
+
+                res.sendStatus(200);
+
+            }
+            else{
+                res.sendStatus(404);
+            }
+    }
+});
