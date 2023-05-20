7a5784eae930371c548183b12739656d21918881
Dinesh Kasnia <dineshx33@gmail.com>
2023-05-20 17:48:50 +0530

    update 14

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get("/webhook", (req, res) => {
     let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
 
-    console.log(req.body, mode, challenge, token);
     if(mode && token){
         if(mode==='subscribe' && token===mytoken){
             res.status(200).send(challenge);
@@ -25,47 +24,46 @@ app.get("/webhook", (req, res) => {
         }
     }
     
-    res.status(200).send('Hello this is webhook setup second');
 });
 
 
-// app.post("/webhook", (req, res) => {
-//     let body_param = req.body;
+app.post("/webhook", (req, res) => {
+    let body_param = req.body;
 
-//     console.log(JSON.stringify(body_param, null, 2));
+    console.log(JSON.stringify(body_param, null, 2));
 
-//     if(body_param.object){
-//         if(body_param.entry && 
-//             body_param.entry[0].changes
-//             ){
-//                 let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-//                 let from = body_param.entry[0].changes[0].value.metadata.display_phone_number;
-//                 let msg_body = body_param.entry[0].changes[0].value.messages ? body_param.entry[0].changes[0].value.messages[0].text.body : "";
+    if(body_param.object){
+        if(body_param.entry && 
+            body_param.entry[0].changes
+            ){
+                let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                let from = body_param.entry[0].changes[0].value.metadata.display_phone_number;
+                let msg_body = body_param.entry[0].changes[0].value.messages ? body_param.entry[0].changes[0].value.messages[0].text.body : "";
 
 
-//                 axios({
-//                     method:'POST',
-//                     url:"https://graph.facebook.com/v16.0/"+phone_number_id+"/messages?access_token="+token,
-//                     data:{
-//                         messaging_product:"whatsapp",
-//                         to:from,
-//                         text:{
-//                             body:"Hi.. I am dinesh kasnia"
-//                         }
-//                     },
-//                     headers:{
-//                         "Content-Type":"application/json"
-//                     }
-//                 });
+                axios({
+                    method:'POST',
+                    url:"https://graph.facebook.com/v16.0/"+phone_number_id+"/messages?access_token="+token,
+                    data:{
+                        messaging_product:"whatsapp",
+                        to:from,
+                        text:{
+                            body:"Hi.. I am dinesh kasnia"
+                        }
+                    },
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                });
 
-//                 res.sendStatus(200);
+                res.sendStatus(200);
 
-//             }
-//             else{
-//                 res.sendStatus(404);
-//             }
-//     }
-// });
+            }
+            else{
+                res.sendStatus(404);
+            }
+    }
+});
 
 app.get("/", (req,res) =>{
     res.status(200).send('Hello this is webhook setup');
